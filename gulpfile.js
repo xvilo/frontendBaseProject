5fcd2a04047efb5a2ea6068cae06b1341aec3ec0
Sem Schilder <sem@tropical.email>
2017-11-14 09:06:49 +0100

    Update gulp tasks and update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -82,7 +82,7 @@ gulp.task('javascripts', function() {
       .pipe(closureCompiler({
           compilation_level: 'ADVANCED',
           warning_level: 'VERBOSE',
-          language_in: 'ECMASCRIPT6_STRICT',
+          language_in: 'ECMASCRIPT6',
           language_out: 'ECMASCRIPT5_STRICT',
           //output_wrapper: '(function(){\n%output%\n}).call(this)',
           js_output_file: 'app.min.js'
